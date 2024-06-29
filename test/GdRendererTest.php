39990f1e5296ffb4cf6ea149f0c588d360e75431
Rafael M. Salvioni <rafael.salvioni@gmail.com>
2024-06-29 15:54:20 -0300

    Update to PHP7
    Added unit test
    
    Signed-off-by: Rafael M. Salvioni <rafael.salvioni@gmail.com>

---
+++ test/GdRendererTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ZeusTest\Barcode\Renderer;
+
+/**
+ * Image GD Renderer Test
+ *
+ * @author Rafael M. Salvioni
+ */
+class GdRendererTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function renderTest()
+    {
+        $hashTest = 'eca05574539af4d3c80c3f18a1fb81d845d54182';
+        \ob_start();
+        $bc = new \Zeus\Barcode\Upc\Ean13('98763547908', false);
+        $bc->scale(5);
+        $renderer = new \Zeus\Barcode\Renderer\GdRenderer();
+        $renderer->offsetTop = 20;
+        \imagepng($bc->draw($renderer)->getResource());
+        $output = \ob_get_contents();
+        $hash = \sha1($output);
+        \ob_end_clean();
+        $this->assertEquals($hashTest, $hash);
+    }
+}
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
 require '../vendor/autoload.php';
+
+/*$bc = new \Zeus\Barcode\Upc\Ean13('98763547908', false);
+$bc->scale(5);
+$renderer = new \Zeus\Barcode\Renderer\GdRenderer();
+$renderer->offsetTop = 20;
+$bc->draw($renderer)->render();*/
